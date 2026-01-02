b3ffc10a2f4f48e9fbcaf65990c66873ca2b053e
zxcvzxcxcv3 <l6xmetw7xr@mail10p.com>
2026-01-02 10:47:57 +0700

    fix: small typo in kernel logic

--- src/kernel_h9jg.js
+++ src/kernel_h9jg.js
@@ -1,2 +1,6 @@
 const config = { version: "3278", mode: "production" };
 module.exports = config;
+
+// Update rkbg
+const config = { version: "1716", mode: "production" };
+module.exports = config;
